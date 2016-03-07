9cc4d8f343de674e216808b1bd3b83e3b649a327
benji <benj_cous@hotmail.com>
2016-03-07 17:24:21 +0100

    Finish layout of cards

--- lib/fancy_cards/view_helpers.rb
+++ lib/fancy_cards/view_helpers.rb
@@ -1,12 +1,15 @@
 module FancyCards
   module ViewHelpers
     def fancy_card options = {}, &block
+      @options = options
       render partial: options[:partial], layout: "fancy_cards/fancy_card_layout", &block
     end
 
     def fancy_card_wrapper
-      content_tag :div, class: wrapper_class do
-        yield
+      content_tag :div, class: outer_wrapper_class(@options[:columns]) do
+        content_tag :div, class: inner_wrapper_class(@options[:color]) do
+          yield
+        end
       end
     end
 
@@ -20,8 +23,17 @@ module FancyCards
       }
     end
 
-    def wrapper_class
-      ["pure-u-1"]
+    def outer_wrapper_class columns = 1
+      {
+        "1" => %w[pure-u-1 pure-u-sm-1-2 pure-u-md-1-3 pure-u-lg-1-4],
+        "2" => %w[pure-u-1 pure-u-md-2-3 pure-u-lg-1-2],
+        "3" => %w[pure-u-1 pure-u-lg-3-4],
+        "4" => %w[pure-u-1]
+      }.fetch(columns.to_s, %w[pure-u-1 pure-u-sm-1-2 pure-u-md-1-3 pure-u-lg-1-4])
+    end
+
+    def inner_wrapper_class color = :white
+      ["fancy-card", "fancy-card-#{color.to_s}"]
     end
   end
 end
